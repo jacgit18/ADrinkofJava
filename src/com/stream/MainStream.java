c64b26463ecea01101990a1109dea5d4c880993e
jacgit18 <joshuaxcarpentier@gmail.com>
2023-02-26 00:45:22 -0500

    added more stream

--- src/com/stream/MainStream.java
+++ src/com/stream/MainStream.java
@@ -1,8 +1,18 @@
 package com.stream;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+//import static java.nio.file.standardCopyOption;
+
 
 public class MainStream {
     public static void main(String[] args) {
@@ -12,8 +22,62 @@ public class MainStream {
 //        doTryWithResorcesMulti();
 //        doCloseThing();
 
+        String[] data = {
+                "Line 1",
+                "Line 2 2",
+                "Line 3 3 3",
+                "Line 4 4 4 4",
+                "Line 5 5 5 5 5",
+        };
+
+        try(FileSystem zipFs = openZip(Paths.get("myData.zip"))){
+            copyToZip(zipFs);
+            writeToFileInZip1(zipFs, data);
+            writeToFileInZip2(zipFs, data);
+
+        }catch (Exception e){
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+
+        }
+
     }
 
+    private static FileSystem openZip(Path zipPath) throws IOException, URISyntaxException {
+
+        Map<String, String> providerProps = new HashMap<>();
+
+        providerProps.put("create", "true");
+        URI zipUri = new URI("jar:file", zipPath.toUri().getPath(), null);
+        FileSystem zipFs = FileSystems.newFileSystem(zipUri, providerProps);
+
+        return zipFs;
+
+    }
+
+
+    private static void copyToZip(FileSystem zipFs) throws IOException {
+
+        // Path sourceFile = Paths.get("file1.txt");
+        Path sourceFile = FileSystems.getDefault().getPath("file1.txt"); // long way
+        Path destFile = zipFs.getPath("/file1Copied.txt");
+
+        Files.copy(sourceFile, destFile, standardCopyOption.REPLACE_EXISTING);// FIX
+    }
+
+    private static void writeToFileInZip1(FileSystem zipFs, String[] data) throws IOException {
+        try(BufferedWriter writer = Files.newBufferedWriter(zipFs.getPath("/newFile1.txt"))){
+          for (String d:data){
+              writer.write(d);
+              writer.newLine();
+          }
+
+        }
+    }
+
+    private static void writeToFileInZip2(FileSystem zipFs, String[] data) throws IOException {
+        Files.write(zipFs.getPath("/newFile2.txt"), Arrays.asList(data),
+                Charset.defaultCharset(), StandardOpenOption.CREATE);
+    }
 
         public static void doTryCatchFinally(){
         // long hand way
